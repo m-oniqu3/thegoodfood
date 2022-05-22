3e469cdfeb7639fa6252c3694329f44ff8e9c236
Monique McIntyre <skaiwilliams85@gmail.com>
2022-05-22 12:32:41 -0500

    Added contact section

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React from "react";
 import "./App.css";
 import About from "./components/about/About";
+import Contact from "./components/contact/Contact";
 import Download from "./components/download/Download";
 import Header from "./components/header/Header";
 import Menu from "./components/menu/Menu";
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Header />
       <About />
       <Download />
       <Menu />
+      <Contact />
     </div>
   );
 }
---
+++ src/components/contact/Contact.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import styled from "./Contact.module.css";
+import Heading from "../header/Heading";
+import Background from "../helpers/Background";
+import Wrapper from "../helpers/Wrapper";
+import greyBg from "../../images/greybg.png";
+import spices from "../../images/spices.png";
+import Button from "../button/Button";
+
+const Contact = () => {
+  const text = (
+    <>
+      get in <span>touch</span> with us
+    </>
+  );
+  return (
+    <Background url={greyBg}>
+      <section className={styled.contact}>
+        <Wrapper>
+          <figure>
+            <img src={spices} alt="Three small containers of spices" />
+          </figure>
+
+          <article>
+            <Heading text={text} className="heading-md" />
+            <p className="paragraph">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur
+              nihil sit consectetur minus dolor culpa. At eos laudantium
+              consequatur tenetur?
+            </p>
+          </article>
+
+          <form className={styled.form}>
+            <label htmlFor="name">Your Name</label>
+            <input type="text" name="name" />
+
+            <label htmlFor="email">Your Email</label>
+            <input type="email" name="email" />
+
+            <label htmlFor="message">Your Message</label>
+            <textarea name="message" rows="5" cols="20" />
+
+            <Button>Send</Button>
+          </form>
+        </Wrapper>
+      </section>
+    </Background>
+  );
+};
+
+export default Contact;
