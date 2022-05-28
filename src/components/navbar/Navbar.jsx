e6ef51f2a3695ef3147ca74ac379927c745ae992
Monique McIntyre <skaiwilliams85@gmail.com>
2022-05-28 08:26:51 -0500

    Added links to navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,20 +1,39 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styled from "./Navbar.module.css";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { HashLink as Link } from "react-router-hash-link";
 
 const Navbar = () => {
+  // state to manage the menu
   const [showNav, setShowNav] = useState(false);
+
+  // function to toggle the menu
   const handleMenu = () => setShowNav((showNav) => !showNav);
+
+  //function to show the menu when the window resizes
+  useEffect(() => {
+    function showItems() {
+      if (window.innerWidth > 700) {
+        setShowNav(true);
+      } else if (showNav) {
+        setShowNav(false);
+      }
+    }
+
+    window.addEventListener("resize", showItems);
+
+    return () => window.removeEventListener("resize", showItems);
+  }, [showNav]);
 
   return (
     <nav className={styled.navbar}>
       <p>the good food</p>
 
-      <div onClick={handleMenu}>
-        <GiHamburgerMenu size="25px" style={{ color: "var(--green)" }} />
+      <div onClick={handleMenu} className={styled.icon}>
+        <GiHamburgerMenu size="25px" style={{ color: "var(--white)" }} />
       </div>
 
+      {/* show menu is showNav is true */}
       {showNav && (
         <ul
           className={styled.links}
