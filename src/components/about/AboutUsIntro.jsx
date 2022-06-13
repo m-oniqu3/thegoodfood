142629f4c1b391736f19870e87352f212a581878
Monique McIntyre <skaiwilliams85@gmail.com>
2022-06-12 20:46:35 -0500

    Started replacing lorem ipsum with content

--- src/components/about/AboutUsIntro.jsx
+++ src/components/about/AboutUsIntro.jsx
@@ -25,15 +25,19 @@ const AboutUsIntro = () => {
               className="heading-md"
             />
             <p className="paragraph">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Faucibus
-              in sit sit faucibus. Tincidunt cras sit viverra curabitur lectus
-              aenean. Ut egestas odio pellentesque aenean. Mattis eu felis
-              porttitor pretium.
+              The Good Food was started by a group of friends who were
+              passionate about food and wanted to share their love of good eats
+              with the world. We believe that everyone deserves access to
+              delicious and healthy meals, so we opened up restaurant to cook
+              and serve up amazing food for everyone to enjoy.
             </p>
 
             <p className="paragraph">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Faucibus
-              in sit sit faucibus. Tincidunt
+              Our menu is always changing, but you can always expect to find
+              fresh, made-from-scratch dishes that will tantalize your taste
+              buds. Whether you're looking for a quick bite or a leisurely meal,
+              The Good Food is the perfect spot for you. So come on in and enjoy
+              some good food!
             </p>
           </article>
         </div>
